432b07cf04d2243c8c6222ac63606da4737bdc10
saji37 <sajiththomas162631@gmail.com>
2022-06-25 18:48:21 +0530

    adding lists

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [todo,setTodo] = useState('')
+  const [todos,setTodos] = useState([])
   return (
     <div className="app">
       <div className="mainHeading">
@@ -11,19 +13,36 @@ function App() {
         <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
       </div>
       <div className="input">
-        <input type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+        <input type="text" value={todo} onChange={(e)=>setTodo(e.target.value)} placeholder="🖊️ Add item..." />
+        <i onClick={()=>setTodos([...todos,{id:Date.now(),text:todo,status:false}])} className="fas fa-plus"></i>
       </div>
       <div className="todos">
+        { todos.map((obj)=>{
+          return(
         <div className="todo">
           <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>Rect tutorial</p>
+          <input onChange={(e)=>{
+           
+            console.log(obj)
+            setTodos(todos.filter(obj2=>{
+              if(obj2.id===obj.id){
+                obj2.status=e.target.checked
+              }
+              console.log(e.target.checked)
+              return obj2
+            }))
+          }}
+            value={obj.status} type="checkbox" name="" id="" />
+            <p>{obj.text}</p>
           </div>
           <div className="right">
             <i className="fas fa-times"></i>
           </div>
         </div>
+    
+            )
+          })
+        }
       </div>
     </div>
   );
